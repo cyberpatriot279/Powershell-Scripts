57ebcb7e4c8fdd2820997bb9bf0363521c33763b
Steve Sanden <ssanden@gaig.com>
2019-10-17 23:23:58 -0400

    added a few scripts

---
+++ stop-insecure-features.ps1
@@ -0,0 +1,11 @@
+﻿
+#Get all running services
+#Get-Service | where {$_.Status -eq 'running'}
+
+## disable features ( IIS / FAX/ FTP / TELNET
+foreach($item in Get-WindowsOptionalFeature -Online | where { ($_.FeatureName -like "*ftp*")  -or  ($_.FeatureName -like "*smb*") -or ($_.FeatureName  -like "*iis*")  -or ($_.FeatureName  -like "*telnet*") -or ($_.FeatureName -like "*fax*")}) {
+
+    #$name=$item.FeatureName
+    Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName $item.FeatureName
+     
+}
---
+++ user-password-reset.ps1
@@ -0,0 +1,14 @@
+﻿## Read a file of users and reset all passwords
+
+foreach($name in Get-Content .\users.txt) {
+
+    $Password = ConvertTo-SecureString -String "P@sSwOrd" -AsPlainText -Force
+    $UserAccount = Get-LocalUser -Name $name
+    $UserAccount | Set-LocalUser -Password $Password
+
+    ## List all the users
+    $UserAccount
+
+}
+
+## Disable user that is not in the file
